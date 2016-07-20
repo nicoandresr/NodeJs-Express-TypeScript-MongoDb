f0f515047319118e3aeaa3cdeee4d5905d5ee9e6
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-07-20 16:31:47 -0500

    Enable mongodb database

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 /// <reference path="typings/tsd.d.ts" />
 
 import * as express from "express";
+import * as mongodb from "mongodb";
 
 module AppBackEnd {
 
@@ -9,15 +10,36 @@ module AppBackEnd {
         private _app: express.Application;
         private _msg: string = "Hello world";
         private _consoleLogStartMsg: string = 'App listening on port: ';
+        private _uri: string = "mongodb://localhost:27017/test";
 
         constructor(port: number) {
             this._app = express();
             this.initExpressServer(port);
         }
 
-        private initExpressServer(port: number){
-            this._app.get('/', (req, res) => { res.send(this._msg) });
+        private initExpressServer(port: number): void {
+            mongodb.MongoClient.connect(this._uri, (error, db) => {
+               this.mongoConnectHandler(error, db, port); 
+            });
+        }
+
+        private mongoConnectHandler(error: mongodb.MongoError, db: mongodb.Db, port: number): void {
+            this.mongoErrorHandler(error);
+            let result: string;
+            db.collection('test').find().toArray((error, docs) => {
+                this.mongoErrorHandler(error);
+                docs.forEach((doc) => {
+                    result += JSON.stringify(doc);
+                });
+            });
+            this._app.get('/', (req, res) => { res.send(result) });
             this._app.listen(port, () => console.log(this._consoleLogStartMsg + port));
+        }
+
+        private mongoErrorHandler(error: mongodb.MongoError): void {
+            if (!error) return;
+            console.log(error);
+            process.exit(1);
         }
     }
 }
